1c8aa13b17e8124bd602df115a9a3e10eda9dfdf
dev-great <gmarshal070@gmail.com>
2025-05-24 21:23:14 +0100

    added

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils.text import slugify
+from ckeditor.fields import RichTextField
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    author = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='blog_images/')
+    content = RichTextField()
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
---
+++ staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/Masco/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+def index(request):
+    return render(request, "index6.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact3.html")
+
+def faq(request):
+    return render(request, "faq.html")
+
+def portfolio(request):
+    return render(request, "portfolio.html")
